90d7e0e0f9323947e80e973c0f64937fdf233ae7
Rami Manan <mananrami09@gmail.com>
2026-04-09 23:38:31 +0530

    Kelvin to Fahrenheit conversion program

---
+++ 19_Kelvin_to_Fahrenheit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>   /* Standard I/O library for printf() and scanf() */
+
+int main()           /* Main function - program execution starts here */
+{
+    float kelvin, fahrenheit;   /* Declare variables to store temperatures */
+
+    /* Ask user to enter temperature in Kelvin */
+    printf("Enter the temperature in Kelvin: ");
+
+    /* Read the Kelvin value entered by user */
+    scanf("%f", &kelvin);
+
+    /* Convert Kelvin to Fahrenheit using formula: F = ((9/5) * (K - 273.15)) + 32 */
+    fahrenheit = ((9.0 / 5) * (kelvin - 273.15)) + 32;
+
+    /* Display the result rounded to 2 decimal places */
+    printf("Fahrenheit = %.2f\n", fahrenheit);
+
+    return 0;   /* Return 0 = program executed successfully */
+}
