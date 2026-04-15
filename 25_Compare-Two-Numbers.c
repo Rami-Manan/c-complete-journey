acdace67be641369043eaba8d780484c10fb613d
Rami Manan <mananrami09@gmail.com>
2026-04-15 22:58:45 +0530

    Add program to compare two integers

---
+++ 25_Compare-Two-Numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>   /* Standard I/O library for printf() and scanf() */
+
+int main()           /* Main function - program execution starts here */
+{
+    int a, b;   /* Declare two integer variables to store user input */
+
+    scanf("%d %d", &a, &b);   /* Read two integers from the user */
+
+    if(a > b)                 /* Check if 'a' is greater than 'b' */
+        printf("A is greater");   /* Print message if condition is true */
+    else
+        printf("B is greater");   /* Print message if condition is false */
+
+    return 0;   /* Return 0 = program executed successfully */
+}
