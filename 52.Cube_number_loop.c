6e17b56715d9cd854b57d21b2d60ea1271c0c8c9
Rami Manan <mananrami09@gmail.com>
2026-05-19 22:49:49 +0530

    DONE

---
+++ 52.Cube_number_loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>   /* Standard I/O library for printf() and scanf() */
+
+int main()           /* Main function - program starts here */
+{
+    int n;           /* Variable to store the limit entered by user */
+
+    /* Ask user to enter the limit */
+    printf("Enter limit: ");
+    scanf("%d", &n);   /* Read integer input and store in n */
+
+    /* Loop from 1 to n and print cube of each number */
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d\t", i * i * i);   /* Calculate and print cube of i, \t for neat spacing */
+    }
+
+    printf("\n");   /* Move to next line after all cubes are printed */
+
+    return 0;   /* Return 0 = program executed successfully */
+}
