dcdc7d75ba8731667273b2c8fac56fb5eeea2ac5
Rami Manan <mananrami09@gmail.com>
2026-06-04 22:54:20 +0530

    Add pattern printing program in C

---
+++ 68.08_Pattern_Loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>   /* Standard I/O library for printf() */
+
+int main()
+{
+    /* Outer loop: controls rows from e to a */
+    for(char i = 'e'; i >= 'a'; i--)
+    {
+        /* Inner loop: prints characters from a to current character */
+        for(char j = 'a'; j <= i; j++)
+        {
+            printf("%c ", j);   /* Print current character */
+        }
+
+        printf("\n");   /* Move to next line */
+    }
+
+    return 0;   /* Program executed successfully */
+}
