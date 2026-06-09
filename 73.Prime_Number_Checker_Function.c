c751c4b5c9460068f50652e4ec958963a2c89ef8
Rami Manan <mananrami09@gmail.com>
2026-06-09 19:43:24 +0530

    Add prime number checker function in C

---
+++ 73.Prime_Number_Checker_Function.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* Returns 1 if prime, 0 if not prime */
+int isPrime(int n)
+{
+    int i;
+
+    if(n <= 1) return 0;   /* 0 and 1 are not prime */
+
+    for(i = 2; i * i <= n; i++)   /* Check divisors up to sqrt(n) */
+    {
+        if(n % i == 0)
+            return 0;   /* Divisible = not prime */
+    }
+    return 1;   /* No divisor found = prime */
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(isPrime(n))
+        printf("%d is a Prime number\n", n);
+    else
+        printf("%d is NOT a Prime number\n", n);
+
+    return 0;
+}
