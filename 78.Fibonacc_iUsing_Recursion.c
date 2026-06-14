6d7959db7b7a4753ea13e57f8a8973856056e2ec
Rami Manan <mananrami09@gmail.com>
2026-06-14 19:09:05 +0530

    Done

---
+++ 78.Fibonacc_iUsing_Recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* Return nth Fibonacci number */
+int fibonacci(int n)
+{
+    if(n == 0) return 0;   /* Base case 1 */
+    if(n == 1) return 1;   /* Base case 2 */
+    return fibonacci(n-1) + fibonacci(n-2);   /* Recursive call */
+}
+
+int main()
+{
+    int n, i;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", fibonacci(i));
+
+    printf("\n");
+    return 0;
+}
