36320bac8e6edfff5bae456053618da1fb3ad383
Rami Manan <mananrami09@gmail.com>
2026-06-25 22:05:38 +0530

    Done

---
+++ 90.String_Length_Using_Pointer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* Count characters using pointer arithmetic */
+int strLenPtr(char *ptr)
+{
+    int count = 0;
+
+    while(*ptr != '\0')   /* Dereference and check for null terminator */
+    {
+        count++;
+        ptr++;            /* Move to next character */
+    }
+
+    return count;
+}
+
+int main()
+{
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    printf("Length = %d\n", strLenPtr(str));
+
+    return 0;
+}
