705d51ede8c38700c2873e4586ebb7a553ad681f
Rami Manan <mananrami09@gmail.com>
2026-06-27 11:35:39 +0530

    Done
    Removed comments explaining the program's purpose.

---
+++ 92.Count_Array_Elements_Using_Pointer..c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {3, 6, 9, 12, 15, 18, 21};
+    int *start = arr;                /* Pointer to first element */
+    int *end   = arr + (sizeof(arr)/sizeof(arr[0]));  /* Pointer past last element */
+
+    /* Pointer subtraction gives number of elements between them */
+    int count = end - start;
+
+    printf("Array  : ");
+    int *ptr;
+    for(ptr = start; ptr < end; ptr++)
+        printf("%d ", *ptr);
+    printf("\n");
+
+    printf("Count  : %d\n", count);
+    printf("Sum    : ");
+
+    int sum = 0;
+    for(ptr = start; ptr < end; ptr++)
+        sum += *ptr;
+
+    printf("%d\n", sum);
+
+    return 0;
+}
