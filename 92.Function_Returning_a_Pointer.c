b7b9ddb1e4a3710b7011de34f1709ef169ea9c1f
Rami Manan <mananrami09@gmail.com>
2026-06-28 21:54:39 +0530

    Done

---
+++ 92.Function_Returning_a_Pointer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* Returns pointer to the larger of two numbers */
+int* findLarger(int *a, int *b)
+{
+    if(*a > *b)
+        return a;   /* Return address of larger value */
+    else
+        return b;
+}
+
+int main()
+{
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    int *larger = findLarger(&x, &y);   /* Get pointer to larger */
+
+    printf("Larger value = %d\n", *larger);   /* Dereference to get value */
+
+    return 0;
+}
