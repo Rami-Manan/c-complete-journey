3ea87e1ee3b8e78d70cdfd5c93ac53219a58b67a
Rami Manan <mananrami09@gmail.com>
2026-07-04 22:33:06 +0530

    Done

---
+++ 98.Store_N_Names_Using_Dynamic_Memory.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int n, i;
+    char **names;       /* Array of string pointers */
+    char temp[100];     /* Temporary input buffer */
+
+    printf("How many names? ");
+    scanf("%d", &n);
+    getchar();          /* Consume leftover newline */
+
+    /* Allocate array of n string pointers */
+    names = (char **)malloc(n * sizeof(char *));
+    if(names == NULL) { printf("Allocation failed!\n"); return 1; }
+
+    for(i = 0; i < n; i++)
+    {
+        printf("Enter name %d: ", i+1);
+        fgets(temp, 100, stdin);
+        temp[strcspn(temp, "\n")] = '\0';   /* Remove trailing newline */
+
+        /* Allocate exact memory for this name */
+        names[i] = (char *)malloc((strlen(temp) + 1) * sizeof(char));
+        strcpy(names[i], temp);
+    }
+
+    printf("\nStored Names:\n");
+    for(i = 0; i < n; i++)
+        printf("%d. %s\n", i+1, names[i]);
+
+    /* Free each name, then free the array */
+    for(i = 0; i < n; i++)
+        free(names[i]);
+    free(names);
+
+    return 0;
+}
