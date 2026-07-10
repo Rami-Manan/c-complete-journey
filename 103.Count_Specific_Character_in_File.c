1ae89d0af5a3599ef61b5b98ec2ea30128b8a9be
Rami Manan <mananrami09@gmail.com>
2026-07-10 23:19:34 +0530

    Done

---
+++ 103.Count_Specific_Character_in_File.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp;
+    char target, ch;
+    int count = 0;
+
+    fp = fopen("diary.txt", "r");
+
+    if(fp == NULL)
+    {
+        printf("File not found!\n");
+        return 1;
+    }
+
+    printf("Enter character to count: ");
+    scanf(" %c", &target);   /* Space before %c skips whitespace */
+
+    while((ch = fgetc(fp)) != EOF)
+    {
+        if(ch == target)
+            count++;
+    }
+
+    fclose(fp);
+
+    printf("'%c' appears %d time(s) in the file\n", target, count);
+
+    return 0;
+}
